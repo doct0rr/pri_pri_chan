7934ba12df541d9f4bf5c244f1426549c2fe10a1
kriss <clevekriss@gmail.com>
2017-08-27 13:46:54 +0530

    verification + heroku add

--- pri_chan.py
+++ pri_chan.py
@@ -6,3 +6,11 @@ import requests
 from flask import Flask, request
 
 app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Does this shit work", 200
